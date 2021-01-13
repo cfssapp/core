a4cbd35efd041b3f2f41151a5e9ba3990cf02046
cfssapp <gohweihan21@gmail.com>
2021-01-13 20:33:47 +0800

    save

--- apps/shopping/views.py
+++ apps/shopping/views.py
@@ -22,3 +22,126 @@ class ItemList(generics.ListAPIView):
         user = self.request.user
         return Item.objects.filter(item_owner=user, cartadded=False, ordered=False).order_by('-id')
 
+class ItemDetail(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class CreateItem(generics.CreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+    def create(self, request, *args, **kwargs):
+        tracking_no = request.data.get('tracking_no', None)
+        item_qs = Item.objects.filter(tracking_no=tracking_no)
+       
+        if item_qs.exists():
+            return Response({"message": "Invalid request"}, status=HTTP_400_BAD_REQUEST)
+
+        serializer = ItemSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save(item_owner=self.request.user)
+        articles = Item.objects.filter(item_owner=self.request.user, ordered=False).order_by('-id')
+        serializer = ItemSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+        
+
+class EditItem(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        articles = Item.objects.filter(item_owner=self.request.user, ordered=False).order_by('-id')
+        serializer = ItemSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+class DeleteItem(generics.RetrieveDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        articles = Item.objects.filter(item_owner=self.request.user, ordered=False).order_by('-id')
+        serializer = ItemSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+
+class AddToCartView(APIView):
+    def post(self, request, *args, **kwargs):
+        tracking_no = request.data.get('tracking_no', None)
+        if tracking_no is None:
+            return Response({"message": "Invalid request"}, status=HTTP_400_BAD_REQUEST)
+
+        item = Item.objects.filter(item_owner=self.request.user, tracking_no=tracking_no)
+        item.update(cartadded=True)
+        
+        articles = Item.objects.filter(item_owner=self.request.user, cartadded=False, ordered=False).order_by('-id')
+        serializer = ItemSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+class RemoveFromCartView(APIView):
+    def post(self, request, *args, **kwargs):
+        tracking_no = request.data.get('tracking_no', None)
+        if tracking_no is None:
+            return Response({"message": "Invalid request"}, status=HTTP_400_BAD_REQUEST)
+
+        item = Item.objects.filter(item_owner=self.request.user, tracking_no=tracking_no)
+        item.update(cartadded=False)  
+
+        articles = Item.objects.filter(item_owner=self.request.user, cartadded=True, ordered=False).order_by('-id')
+        serializer = ItemSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+class CartList(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Item.objects.filter(item_owner=user, cartadded=True, ordered=False).order_by('-id')
+
+class OrderList(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(user=user).order_by('-id')
+
+class AddToOrderView(APIView):
+    def post(self, request, *args, **kwargs):
+        shipping_address = request.data.get('shipping_address', None)
+
+        order = Order.objects.create(
+            user=self.request.user,
+            shipping_address=shipping_address
+        )
+
+        cartadded_items = Item.objects.filter(item_owner=self.request.user, cartadded=True)
+        for item in cartadded_items:
+            order.items.add(item)
+    
+        cartadded_items.update(ordered=True)
+        cartadded_items.update(cartadded=False)
+        for item in cartadded_items:
+            item.save()
+
+        articles = Item.objects.filter(item_owner=self.request.user, cartadded=True, ordered=False).order_by('-id')
+        serializer = ItemSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
