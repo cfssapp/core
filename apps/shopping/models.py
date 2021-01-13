945c6eda2a01b1bb3654a90967156c4c4939f869
cfssapp <gohweihan21@gmail.com>
2021-01-13 18:35:16 +0800

    save

--- apps/shopping/models.py
+++ apps/shopping/models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Item(models.Model):
+    tracking_no = models.CharField(max_length=100)
+    weight = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    remark = models.CharField(max_length=100)
+    item_owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='item', default=1)
+    ordered = models.BooleanField(default=False)
+    cartadded = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.tracking_no
+
+
+class Order(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='order', default=1)
+    items = models.ManyToManyField(Item)
+    shipping_address = models.CharField(max_length=100, default="not set")
+    
+    
+
--- apps/shopping/views.py
+++ apps/shopping/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
-from rest_framework.decorators import api_view
+from django.http import JsonResponse, HttpResponse
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework import viewsets, permissions
+
+from .serializers import ItemSerializer, OrderItemSerializer, OrderSerializer
+from .models import Item, OrderItem, Order
+
+from rest_framework.views import APIView
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
 # Create your views here.
+class ItemList(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ItemSerializer
 
-@api_view(['GET'])
-def apiOverview(request):
-	api_urls = {
-		'List':'/task-list/',
-		'Detail View':'/task-detail/<str:pk>/',
-		'Create':'/task-create/',
-		'Update':'/task-update/<str:pk>/',
-		'Delete':'/task-delete/<str:pk>/',
-		}
+    def get_queryset(self):
+        user = self.request.user
+        return Item.objects.filter(item_owner=user, cartadded=False, ordered=False).order_by('-id')
 
-	return Response(api_urls)
