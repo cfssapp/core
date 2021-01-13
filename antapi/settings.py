58fb9697b7715bc471f747fa1761bf09cacb6d4e
cfssapp <gohweihan21@gmail.com>
2021-01-13 17:53:20 +0800

    save

--- antapi/settings.py
+++ antapi/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'https://drfserver.pythonanywhere.com/media/'
+MEDIA_URL = 'https://antapi.pythonanywhere.com/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
