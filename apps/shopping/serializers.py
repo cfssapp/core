6e12995e2cd1ea948981cee10eb86e32e72f0ea4
cfssapp <gohweihan21@gmail.com>
2021-01-13 18:37:00 +0800

    save

---
+++ apps/shopping/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Item, Order
+
+class ItemSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Item
+        fields ='__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = ItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields ='__all__'
