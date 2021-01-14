54f24f8fefac0add11bc7e323dbe4cdf50fe00d8
cfssapp <gohweihan21@gmail.com>
2021-01-14 12:51:03 +0800

    save

--- apps/shopping/models.py
+++ apps/shopping/models.py
@@ -11,10 +11,11 @@ class Item(models.Model):
     price = models.CharField(max_length=100)
     remark = models.CharField(max_length=100)
     item_owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='item', default=1)
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owneritem', default=1)
     ordered = models.BooleanField(default=False)
     cartadded = models.BooleanField(default=False)
-    order_id = models.ForeignKey('Order', on_delete=models.CASCADE, blank=True, null=True)
+    # order_id = models.ForeignKey('Order', on_delete=models.CASCADE, blank=True, null=True)
+    order_id = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.tracking_no
