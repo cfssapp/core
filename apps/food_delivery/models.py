f7d2f1fa0c3292bf6020c67a2c960e1fa477916f
cfssapp <gohweihan21@gmail.com>
2021-01-21 16:04:35 +0800

    save

--- apps/food_delivery/models.py
+++ apps/food_delivery/models.py
@@ -14,7 +14,8 @@ class FoodAvatar(models.Model):
 
 
 class FoodItem(models.Model):
-    avatar = models.ForeignKey('FoodAvatar', on_delete=models.CASCADE, blank=True, null=True)
+    # avatar = models.ForeignKey('FoodAvatar', on_delete=models.CASCADE, blank=True, null=True)
+    avatar = models.OneToOneField(FoodAvatar, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=100, default="not set")
     price = models.CharField(max_length=100, default="not set")
     category = models.CharField(max_length=100, default="not set")
