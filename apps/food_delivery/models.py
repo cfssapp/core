054336b98104a28d482e248b572d84640e843c36
cfssapp <gohweihan21@gmail.com>
2021-01-21 13:50:57 +0800

    save

--- apps/food_delivery/models.py
+++ apps/food_delivery/models.py
@@ -8,7 +8,8 @@ class FoodAvatar(models.Model):
     file = models.ImageField(default='default.jpg', upload_to='upload_pics')
 
     def __str__(self):
-        return str(self.file)
+        # return str(self.file)
+        return f"https://antapi.pythonanywhere.com/media/{str(self.file)}"
 
 
 class FoodItem(models.Model):
