c58eb27b1edd07bb99641dc2a1b53d7a81719d8b
cfssapp <gohweihan21@gmail.com>
2021-01-21 13:18:45 +0800

    save

--- apps/food_delivery/models.py
+++ apps/food_delivery/models.py
@@ -8,7 +8,7 @@ class FoodAvatar(models.Model):
     file = models.ImageField(default='default.jpg', upload_to='upload_pics')
 
     def __str__(self):
-        return self.file
+        return self.id
 
 
 class FoodItem(models.Model):
