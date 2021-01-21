16769431459beb536ed27b41666c14e16fde8e9f
cfssapp <gohweihan21@gmail.com>
2021-01-21 13:11:35 +0800

    save

--- apps/food_delivery/models.py
+++ apps/food_delivery/models.py
@@ -4,8 +4,15 @@ from django.conf import settings
 
 
 # Create your models here.
+class FoodAvatar(models.Model):
+    file = models.ImageField(default='default.jpg', upload_to='upload_pics')
+
+    def __str__(self):
+        return self.file
+
+
 class FoodItem(models.Model):
-    avatar = models.ImageField(default='default.jpg', upload_to='upload_pics')
+    avatar = models.ForeignKey('FoodAvatar', on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=100, default="not set")
     price = models.CharField(max_length=100, default="not set")
     category = models.CharField(max_length=100, default="not set")
