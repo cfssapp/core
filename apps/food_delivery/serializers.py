55a7ee1024bfa842ffa418481a941804f0e28759
cfssapp <gohweihan21@gmail.com>
2021-01-21 16:15:33 +0800

    save

--- apps/food_delivery/serializers.py
+++ apps/food_delivery/serializers.py
@@ -24,11 +24,11 @@ class FoodItemSerializer(serializers.ModelSerializer):
         model = FoodItem
         fields ='__all__'
 
-    def to_representation(self, instance):
-        rep = super(FoodItemSerializer, self).to_representation(instance)
-        # rep['avatar'] = FoodAvatarSerializer(instance.avatar.file).data
-        rep['avatar'] = FoodItemSerializer(instance.file).data
-        return rep
+    # def to_representation(self, instance):
+    #     rep = super(FoodItemSerializer, self).to_representation(instance)
+    #     # rep['avatar'] = FoodAvatarSerializer(instance.avatar.file).data
+    #     rep['avatar'] = FoodItemSerializer(instance.file).data
+    #     return rep
 
 
 class FoodOrderSerializer(serializers.ModelSerializer):
