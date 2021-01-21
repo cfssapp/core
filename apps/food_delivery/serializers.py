80c99a146c6cbd276261f07673e18db06d9f5923
cfssapp <gohweihan21@gmail.com>
2021-01-21 14:02:41 +0800

    save

--- apps/food_delivery/serializers.py
+++ apps/food_delivery/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import FoodItem, FoodOrder, FoodAvatar
 
 class FoodAvatarSerializer(serializers.ModelSerializer):
-    file = serializers.ImageField(required=False, max_length=None, allow_empty_file=True, use_url=True)
+    # file = serializers.ImageField(required=False, max_length=None, allow_empty_file=True, use_url=True)
     class Meta:
         model = FoodAvatar
         fields ='__all__'
 
 class FoodItemSerializer(serializers.ModelSerializer):
-    avatar = FoodAvatarSerializer()
+    avatar = FoodAvatarSerializer(many=True, read_only=True)
 
     class Meta:
         model = FoodItem
