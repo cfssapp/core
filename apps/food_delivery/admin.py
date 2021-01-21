6cbf7cc099e959044d899c1d2c3db0581259f0eb
cfssapp <gohweihan21@gmail.com>
2021-01-21 13:17:43 +0800

    save

--- apps/food_delivery/admin.py
+++ apps/food_delivery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import FoodItem, FoodOrder
+from .models import FoodItem, FoodOrder, FoodAvatar
 
 # Register your models here.
 admin.site.register(FoodAvatar)
