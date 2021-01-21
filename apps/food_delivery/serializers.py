a4ccf5527841e1653c48150f42812beab13b81b9
cfssapp <gohweihan21@gmail.com>
2021-01-21 15:05:44 +0800

    save

--- apps/food_delivery/serializers.py
+++ apps/food_delivery/serializers.py
@@ -10,7 +10,7 @@ class FoodAvatarSerializer(serializers.ModelSerializer):
 class FoodItemSerializer(serializers.ModelSerializer):
     
     # avatar = FoodAvatarSerializer(read_only=True)
-    avatar = serializers.CharField(source='FoodAvatar.file')
+    avatar = serializers.ImageField(source='FoodAvatar.file')
 
     class Meta:
         model = FoodItem
