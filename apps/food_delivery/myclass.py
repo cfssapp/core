da8fdc2e61a57762d8c22162c8ac7f67b6979aea
cfssapp <gohweihan21@gmail.com>
2021-01-25 16:41:55 +0800

    save

--- apps/food_delivery/myclass.py
+++ apps/food_delivery/myclass.py
@@ -3,6 +3,10 @@ import random
 
 # 根据当前时间随机生成流水单号
 class OrderCode:
-    order_no = "K123"
-    print(order_no)
 
+    def __init__(self):
+        self.serialnum = 'K123'
+
+if __name__ == '__main__':
+    obj = OrderCode()
+    print(obj.serialnum)
