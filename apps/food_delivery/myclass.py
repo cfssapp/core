dd06acd5567e5c8078b439b38045de7691700e5a
cfssapp <gohweihan21@gmail.com>
2021-01-25 17:17:00 +0800

    save

--- apps/food_delivery/myclass.py
+++ apps/food_delivery/myclass.py
@@ -3,5 +3,11 @@ import random
 
 
 def unique_order_no_generator(instance):
-    order_new_id = "K123"
+    
+    dt = datetime.now()
+    serialnum = '%04d%02d%02d%02d%02d%02d' % (dt.year,dt.month,dt.day,dt.hour,dt.minute,dt.second)
+    ran_list = random.randint(0,9999)
+    serialnum += '%04d' % (ran_list)
+
+    order_new_id = serialnum
     return order_new_id
