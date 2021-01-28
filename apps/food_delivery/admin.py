b5f1892e99a6f3c9d4ab463a28a4055145037217
cfssapp <gohweihan21@gmail.com>
2021-01-28 10:26:48 +0800

    save

--- apps/food_delivery/admin.py
+++ apps/food_delivery/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import FoodItem, FoodOrder, FoodAvatar, Address, Csv
+
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
 
-# Register your models here.
+
+class FoodItemResource(resources.ModelResource):
+
+    class Meta:
+        model = FoodItem
+
+
 class FoodItemAdmin(ImportExportModelAdmin):
-    resource_class = FoodItem
+    resource_class = FoodItemResource
 
 admin.site.register(FoodAvatar)
 admin.site.register(FoodItem, FoodItemAdmin)
