44545bf1b16ab2578e7f4fbaae5860d614c90225
cfssapp <gohweihan21@gmail.com>
2021-01-29 09:21:30 +0800

    save

--- apps/food_delivery/views.py
+++ apps/food_delivery/views.py
@@ -312,48 +312,6 @@ def fake_data(request):
             "y": 881
         }
     ],
-    "offlineData": [
-        {
-            "name": "Juru Autocity",
-            "cvr": 0.9
-        },
-        {
-            "name": "Stores 1",
-            "cvr": 0.5
-        },
-        {
-            "name": "Stores 2",
-            "cvr": 0.5
-        },
-        {
-            "name": "Stores 3",
-            "cvr": 0.9
-        },
-        {
-            "name": "Stores 4",
-            "cvr": 0.2
-        },
-        {
-            "name": "Stores 5",
-            "cvr": 0.5
-        },
-        {
-            "name": "Stores 6",
-            "cvr": 0.8
-        },
-        {
-            "name": "Stores 7",
-            "cvr": 0.8
-        },
-        {
-            "name": "Stores 8",
-            "cvr": 0.5
-        },
-        {
-            "name": "Stores 9",
-            "cvr": 0.8
-        }
-    ],
 }
 
 	return JsonResponse(api_urls, safe=False)
