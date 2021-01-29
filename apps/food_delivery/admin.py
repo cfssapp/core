3789583a3970a5312175076565e3670f57a3262d
cfssapp <gohweihan21@gmail.com>
2021-01-29 14:41:32 +0800

    save

--- apps/food_delivery/admin.py
+++ apps/food_delivery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import FoodItem, FoodOrder, FoodAvatar, Address, Csv, SalesData
+from .models import FoodItem, FoodOrder, FoodAvatar, Address, Csv, SalesData, FakeData
 
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
