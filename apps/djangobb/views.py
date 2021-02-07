d41580a6cf6c8e626d6ef6e8a1a3a0bfd6fce747
cfssapp <gohweihan21@gmail.com>
2021-02-07 14:52:37 +0800

    save

--- apps/djangobb/views.py
+++ apps/djangobb/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 
-from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
-from rest_framework.response import Response
-from rest_framework import generics
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
-from rest_framework import viewsets, permissions
-from rest_framework.views import APIView
-
-
 
 # Create your views here.
 def fake_data_01(request):
@@ -18,50 +10,6 @@ def fake_data_01(request):
             "x": "Jan",
             "y": 1
         },
-        {
-            "x": "Feb",
-            "y": 2
-        },
-        {
-            "x": "Mar",
-            "y": 3
-        },
-        {
-            "x": "Apr",
-            "y": 4
-        },
-        {
-            "x": "May",
-            "y": 5
-        },
-        {
-            "x": "Jun",
-            "y": 6
-        },
-        {
-            "x": "Jul",
-            "y": 7
-        },
-        {
-            "x": "Aug",
-            "y": 8
-        },
-        {
-            "x": "Sep",
-            "y": 9
-        },
-        {
-            "x": "Oct",
-            "y": 10
-        },
-        {
-            "x": "Nov",
-            "y": 11
-        },
-        {
-            "x": "Dec",
-            "y": 12
-        }
     ],
 }
 
