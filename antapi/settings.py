86e32f2e2256b051090594a2f6df132d5214333d
cfssapp <gohweihan21@gmail.com>
2021-02-07 14:25:58 +0800

    save

--- antapi/settings.py
+++ antapi/settings.py
@@ -85,10 +85,20 @@ WSGI_APPLICATION = 'antapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'antapi$mysql01',
+        'USER': 'antapi',
+        'PASSWORD': 'wh123456',
+        'HOST': 'antapi.mysql.pythonanywhere-services.com',
     }
 }
 
