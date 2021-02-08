80a0e7e50c9e1be65aa961e952e8bb2a732c29db
cfssapp <gohweihan21@gmail.com>
2021-02-08 11:55:19 +0800

    save

--- apps/djangobb/models.py
+++ apps/djangobb/models.py
@@ -3,13 +3,14 @@ from django.conf import settings
 
 
 class Topic(models.Model):
-    # forum = models.ForeignKey(Forum, related_name='topics', verbose_name=_('Forum'))
-    name = models.CharField(_('Subject'), max_length=255)
-    created = models.DateTimeField(_('Created'), auto_now_add=True)
-    updated = models.DateTimeField(_('Updated'), null=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'))
-    views = models.IntegerField(_('Views count'), blank=True, default=0)
-    post_count = models.IntegerField(_('Post count'), blank=True, default=0)
+    forum = models.CharField(max_length=100, default="not set")
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    views = models.IntegerField(blank=True, default=0)
+    post_count = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.name
