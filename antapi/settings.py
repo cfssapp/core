6ad5645995618230410f6dc9f40e4fe1639f0eaf
cfssapp <gohweihan21@gmail.com>
2021-02-08 17:35:29 +0800

    save

--- antapi/settings.py
+++ antapi/settings.py
@@ -128,13 +128,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Etc/GMT+8'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
+
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
