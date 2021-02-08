bf3c30815a2d43dcea4c72743d1ea281f1b00a0e
cfssapp <gohweihan21@gmail.com>
2021-02-08 16:58:20 +0800

    save

--- apps/djangobb/models.py
+++ apps/djangobb/models.py
@@ -20,5 +20,5 @@ class Topic(models.Model):
 
     def save(self, *args, **kwargs):
         if self.created is not None:
-        self.updated = datetime.now()
+            self.updated = datetime.now()
         super(Topic, self).save(*args, **kwargs)
