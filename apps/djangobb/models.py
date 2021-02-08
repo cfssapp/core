dae3800d5ad41956240cd094a635ce31464e74f2
cfssapp <gohweihan21@gmail.com>
2021-02-08 16:41:30 +0800

    save

--- apps/djangobb/models.py
+++ apps/djangobb/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+from django.utils import timezone
+
 
 class Topic(models.Model):
     forum = models.CharField(max_length=100, default="not set")
@@ -14,3 +16,8 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+    if self.updated is None:
+        self.updated = timezone.now()
+    super(Model, self).save(*args, **kwargs)
