354671d5296caf762653bb9a9347c5d5b2f6db3b
cfssapp <gohweihan21@gmail.com>
2021-02-08 13:11:07 +0800

    save

--- apps/djangobb/urls.py
+++ apps/djangobb/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('fake-data-01/', views.fake_data_01, name="datafake01"),
 
     path('topic/', views.TopicList.as_view(), name='listtopic'),
-    path('topic/<int:pk>/', views.FoodItemDetail.as_view(), name='detailtopic'),
-    path('topic/create/', views.CreateFoodItem.as_view(), name='topiccreate'),
+    path('topic/<int:pk>/', views.TopicDetail.as_view(), name='detailtopic'),
+    path('topic/create/', views.CreateTopic.as_view(), name='topiccreate'),
 
 ]
