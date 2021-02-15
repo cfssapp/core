6e1760f0e2d7cc7360f78b1585c4aeab00654bb2
cfssapp <gohweihan21@gmail.com>
2021-02-15 10:06:18 +0800

    save

--- apps/djangobb/serializers.py
+++ apps/djangobb/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
-from .models import Topic
+from .models import Topic, Post
 from users.serializers import CustomUserSerializer
+
+class PostSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields ='__all__'
 
 
 class TopicSerializer(serializers.ModelSerializer):
     user = CustomUserSerializer(read_only=True)
+    posts = PostSerializer(read_only=True)
 
     class Meta:
         model = Topic
