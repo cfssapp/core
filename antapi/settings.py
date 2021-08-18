1d21b68172c4c0b3f4442744e76c15a6c6c8bf22
cfssapp <gohweihan21@gmail.com>
2021-08-18 18:46:18 +0800

    save

--- antapi/settings.py
+++ antapi/settings.py
@@ -173,7 +173,7 @@ AUTH_USER_MODEL = "users.NewUser"
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
