daa2fcecac40d8eb6757b678fc62e3456e814796
cfssapp <gohweihan21@gmail.com>
2021-08-29 11:23:26 +0800

    save

--- apps/calibration/admin.py
+++ apps/calibration/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Certificate
+from .models import Certificate, Comment
 
 # Register your models here.
 admin.site.register(Certificate)
+admin.site.register(Comment)
