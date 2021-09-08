8272d8d56018fa4ebf3b63e8d5589a3a0982be53
cfssapp <gohweihan21@gmail.com>
2021-09-08 09:59:54 +0800

    save

--- api/views.py
+++ api/views.py
@@ -39,56 +39,42 @@ api_urls = {
 }
 
 api_urls1 = {
-    "data": {
-        "name": "Serati Ma",
-        "avatar": "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
-        "userid": "00000001",
-        "email": "antdesign@alipay.com",
-        "signature": "海纳百川，有容乃大",
-        "title": "交互专家",
-        "group": "蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED",
-        "tags": [
-            {
-                "key": "0",
-                "label": "很有想法的"
+    "data": [
+        {
+            "id": "trend-1",
+            "updatedAt": "2021-08-20T11:49:35.376Z",
+            "user": {
+                "name": "曲丽丽",
+                "avatar": "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png"
             },
-            {
-                "key": "1",
-                "label": "专注设计"
+            "group": {
+                "name": "高逼格设计天团",
+                "link": "http://github.com/"
             },
-            {
-                "key": "2",
-                "label": "辣~"
+            "project": {
+                "name": "六月迭代",
+                "link": "http://github.com/"
             },
-            {
-                "key": "3",
-                "label": "大长腿"
-            },
-            {
-                "key": "4",
-                "label": "川妹子"
-            },
-            {
-                "key": "5",
-                "label": "海纳百川"
-            }
-        ],
-        "notifyCount": 12,
-        "unreadCount": 11,
-        "country": "China",
-        "geographic": {
-            "province": {
-                "label": "浙江省",
-                "key": "330000"
-            },
-            "city": {
-                "label": "杭州市",
-                "key": "330100"
-            }
+            "template": "在 @{group} 新建项目 @{project}"
         },
-        "address": "西湖区工专路 77 号",
-        "phone": "0752-268888888"
-    }
+        {
+            "id": "trend-2",
+            "updatedAt": "2021-08-20T11:49:35.376Z",
+            "user": {
+                "name": "付小小",
+                "avatar": "https://gw.alipayobjects.com/zos/rmsportal/cnrhVkzwxjPwAaCfPbdc.png"
+            },
+            "group": {
+                "name": "高逼格设计天团",
+                "link": "http://github.com/"
+            },
+            "project": {
+                "name": "六月迭代",
+                "link": "http://github.com/"
+            },
+            "template": "在 @{group} 新建项目 @{project}"
+        },
+    ]
 }
 
 @api_view(['GET'])
