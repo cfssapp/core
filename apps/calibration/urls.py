8f85275e39c04fce9c7810b54c01df2222c81c27
cfssapp <gohweihan21@gmail.com>
2021-09-10 10:21:49 +0800

    save

--- apps/calibration/urls.py
+++ apps/calibration/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.apiOverview, name="api-overview"),
+    path('', views.apiOverview, name="api-overview"),
 
     path('certificate/', views.CertificateList.as_view(), name='listcertificate'),
     path('certificate/<int:pk>/', views.CertificateDetail.as_view(), name='detailcertificate'),
