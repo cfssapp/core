ba26d5beb6395dfc82f5fd1dd1395803767f658f
cfssapp <gohweihan21@gmail.com>
2023-01-08 08:57:10 +0800

    save

--- apps/shopee/urls.py
+++ apps/shopee/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.apiOverview, name="api-overview"),
-    path('', views.CartViewset, name='json-view'),
+    path('', views.CartViewset.as_view(), name='json-view'),
 
     path('cart/', views.CartList.as_view(), name='listcart'),
     path('cart/<int:pk>/', views.CartDetail.as_view(), name='detailcertificate'),
