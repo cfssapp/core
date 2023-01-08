f61e676744d81057b0460250c9f028af885fcc5a
cfssapp <gohweihan21@gmail.com>
2023-01-08 09:36:45 +0800

    save

--- apps/shopee/views.py
+++ apps/shopee/views.py
@@ -15,6 +15,67 @@ from rest_framework.decorators import api_view
 import json
 
 
+# api_urls = {
+#     "success": True,
+#     "data": {
+#         "list": [
+#     {
+#         "id": 5,
+#         "avatar": {
+#             "id": 43,
+#             "imagefile": "https://antapi.pythonanywhere.com/media/upload_pics/2509_SDg7PyR.png"
+#         },
+#         "name": "100 Plus",
+#         "price": "RM4.53",
+#         "category": "Beverages",
+#         "cartadded": False,
+#         "ordered": False,
+#         "order_id": ""
+#     },
+#     {
+#         "id": 3,
+#         "avatar": {
+#             "id": 41,
+#             "imagefile": "https://antapi.pythonanywhere.com/media/upload_pics/1003_4Htt9rm.png"
+#         },
+#         "name": "Double Cheeseburger",
+#         "price": "RM10.37",
+#         "category": "Burgers",
+#         "cartadded": False,
+#         "ordered": False,
+#         "order_id": ""
+#     },
+#     {
+#         "id": 2,
+#         "avatar": {
+#             "id": 39,
+#             "imagefile": "https://antapi.pythonanywhere.com/media/upload_pics/2100_03CcJBm.png"
+#         },
+#         "name": "Coca-Cola",
+#         "price": "RM4.53",
+#         "category": "Beverages",
+#         "cartadded": False,
+#         "ordered": False,
+#         "order_id": ""
+#     },
+#     {
+#         "id": 1,
+#         "avatar": {
+#             "id": 40,
+#             "imagefile": "https://antapi.pythonanywhere.com/media/upload_pics/1028_IwVvaDb.png"
+#         },
+#         "name": "McChicken",
+#         "price": "RM8.49",
+#         "category": "Burgers",
+#         "cartadded": False,
+#         "ordered": False,
+#         "order_id": ""
+#     }
+# ]
+#     },
+#     "errorCode": 0
+# }
+
 api_urls = {
     "success": True,
     "data": {
@@ -32,51 +93,10 @@ api_urls = {
         "ordered": False,
         "order_id": ""
     },
-    {
-        "id": 3,
-        "avatar": {
-            "id": 41,
-            "imagefile": "https://antapi.pythonanywhere.com/media/upload_pics/1003_4Htt9rm.png"
-        },
-        "name": "Double Cheeseburger",
-        "price": "RM10.37",
-        "category": "Burgers",
-        "cartadded": False,
-        "ordered": False,
-        "order_id": ""
-    },
-    {
-        "id": 2,
-        "avatar": {
-            "id": 39,
-            "imagefile": "https://antapi.pythonanywhere.com/media/upload_pics/2100_03CcJBm.png"
-        },
-        "name": "Coca-Cola",
-        "price": "RM4.53",
-        "category": "Beverages",
-        "cartadded": False,
-        "ordered": False,
-        "order_id": ""
-    },
-    {
-        "id": 1,
-        "avatar": {
-            "id": 40,
-            "imagefile": "https://antapi.pythonanywhere.com/media/upload_pics/1028_IwVvaDb.png"
-        },
-        "name": "McChicken",
-        "price": "RM8.49",
-        "category": "Burgers",
-        "cartadded": False,
-        "ordered": False,
-        "order_id": ""
-    }
 ]
     },
     "errorCode": 0
 }
-
-
 
 @api_view(['GET'])
 def apiOverview(request):
