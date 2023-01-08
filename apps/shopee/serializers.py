0acc27c9b63ed237224a70b106932f8a162c8c49
cfssapp <gohweihan21@gmail.com>
2023-01-08 09:21:58 +0800

    save

--- apps/shopee/serializers.py
+++ apps/shopee/serializers.py
@@ -13,7 +13,9 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class CartSerializer(serializers.ModelSerializer):
     user = CustomUserSerializer(read_only=True)
-    list = ProductSerializer(many=True, read_only=True)
+    products = ProductSerializer(many=True, read_only=True)
+
+    list = serializers.CharField(source='products')
 
     class Meta:
         model = Cart
