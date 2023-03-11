fe1569884bba2a8ece632151ab001e07f21e4a9d
cfssapp <gohweihan21@gmail.com>
2023-03-11 20:24:05 +0800

    save

--- apps/calibration/urls.py
+++ apps/calibration/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
 
     path('certificate/', views.CertificateList.as_view(), name='listcertificate'),
-    path('certificate/<int:pk>/', views.CertificateDetail.as_view(), name='detailcertificate'),
+    # path('certificate/<int:pk>/', views.CertificateDetail.as_view(), name='detailcertificate'),
+
+    path('certificate/<int:certificate_id2>/', views.CertificateDetail.as_view(), name='detailcertificate'),
 
     path('comment-to-certificate/', views.CommentToCertificateView.as_view(), name='commentaddtocertificate'),
 
