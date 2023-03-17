a2c1b82c9406ce423c155a46c4ae82b3b71713c2
cfssapp <gohweihan21@gmail.com>
2023-03-17 15:03:40 +0800

    save

--- apps/calibration/views.py
+++ apps/calibration/views.py
@@ -83,7 +83,7 @@ class CertificateDetail(generics.RetrieveAPIView):
     lookup_field = 'certificate_id'
 
 
-class CommentToCertificateView2(APIView):
+class CommentToCertificateView(APIView):
     # permission_classes = [permissions.IsAuthenticated]
     serializer_class = CertificateSerializer
     queryset = Certificate.objects.all()
@@ -126,48 +126,6 @@ class CommentToCertificateView2(APIView):
         serializer = CertificateSerializer(articles)
         return JsonResponse(serializer.data, safe=False)
     
-class CommentToCertificateView(APIView):
-    # permission_classes = [permissions.IsAuthenticated]
-    serializer_class = CertificateSerializer
-    queryset = Certificate.objects.all()
-
-    def post(self, request, *args, **kwargs):
-        topic_id = request.data.get('id')
-        post_content = request.data.get('content')
-        
-        avatar_id = request.data.get('imagesupload_id')
-        # avatar_get = CommentImage.objects.get(id=avatar_id)
-
-        # comment_user = Certificate.objects.filter(id=topic_id)
-
-
-        new_post = Comment.objects.create(
-            # user=self.request.user,
-            content=post_content,
-            cert_id=topic_id,
-        )
-
-        for imageid in avatar_id:
-            avatar_get = CommentImage.objects.get(id=imageid)
-            new_post.image.add(avatar_get)
-
-        order_qs = Certificate.objects.filter(id=topic_id).order_by('-id').first()
-        order_qs.comments.add(new_post)
-
-        
-        # create activity
-        new_activity = Activity.objects.create(
-            user=self.request.user,
-            group=post_content,
-            project=topic_id,
-            certificate=order_qs,
-            template='commented on @{certificate}.'
-        )
-
-
-        articles = Certificate.objects.get(id=topic_id)
-        serializer = CertificateSerializer(articles)
-        return JsonResponse(serializer.data, safe=False)
 
 
 class CreateCommentImage(generics.CreateAPIView):
