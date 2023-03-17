d1fee375afdef361f2bfdaad5aeb2697cf170466
cfssapp <gohweihan21@gmail.com>
2023-03-17 14:58:19 +0800

    save

--- apps/calibration/views.py
+++ apps/calibration/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenti
 from rest_framework import viewsets, permissions
 
 from .serializers import CertificateSerializer, CommentSerializer, CommentImageSerializer, ActivitySerializer
-from .models import Certificate, Comment, CommentImage, Activity
+from .models import Certificate, Comment, CommentImage, Activity, Comment2
 
 from rest_framework.views import APIView
 from django.shortcuts import render, get_object_or_404
@@ -100,6 +100,49 @@ class CommentToCertificateView(APIView):
 
         new_post = Comment.objects.create(
             user=self.request.user,
+            content=post_content,
+            cert_id=topic_id,
+        )
+
+        for imageid in avatar_id:
+            avatar_get = CommentImage.objects.get(id=imageid)
+            new_post.image.add(avatar_get)
+
+        order_qs = Certificate.objects.filter(id=topic_id).order_by('-id').first()
+        order_qs.comments.add(new_post)
+
+        
+        # create activity
+        new_activity = Activity.objects.create(
+            user=self.request.user,
+            group=post_content,
+            project=topic_id,
+            certificate=order_qs,
+            template='commented on @{certificate}.'
+        )
+
+
+        articles = Certificate.objects.get(id=topic_id)
+        serializer = CertificateSerializer(articles)
+        return JsonResponse(serializer.data, safe=False)
+    
+class CommentToCertificateView2(APIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CertificateSerializer
+    queryset = Certificate.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        topic_id = request.data.get('id')
+        post_content = request.data.get('content')
+        
+        avatar_id = request.data.get('imagesupload_id')
+        # avatar_get = CommentImage.objects.get(id=avatar_id)
+
+        # comment_user = Certificate.objects.filter(id=topic_id)
+
+
+        new_post = Comment2.objects.create(
+            # user=self.request.user,
             content=post_content,
             cert_id=topic_id,
         )
