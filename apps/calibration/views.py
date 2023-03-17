e56ecce0448a32d2f1b26f338c25a68f806847fc
cfssapp <gohweihan21@gmail.com>
2023-03-17 14:45:01 +0800

    save

--- apps/calibration/views.py
+++ apps/calibration/views.py
@@ -99,7 +99,7 @@ class CommentToCertificateView(APIView):
 
 
         new_post = Comment.objects.create(
-            # user=self.request.user,
+            user=self.request.user,
             content=post_content,
             cert_id=topic_id,
         )
@@ -113,13 +113,13 @@ class CommentToCertificateView(APIView):
 
         
         # create activity
-        new_activity = Activity.objects.create(
-            user=self.request.user,
-            group=post_content,
-            project=topic_id,
-            certificate=order_qs,
-            template='commented on @{certificate}.'
-        )
+        # new_activity = Activity.objects.create(
+        #     user=self.request.user,
+        #     group=post_content,
+        #     project=topic_id,
+        #     certificate=order_qs,
+        #     template='commented on @{certificate}.'
+        # )
 
 
         articles = Certificate.objects.get(id=topic_id)
