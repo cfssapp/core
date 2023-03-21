24eb4608c4f57a99ff7e6561cc0caeb04e60f92e
cfssapp <gohweihan21@gmail.com>
2023-03-22 06:10:31 +0800

    save

--- apps/telegram/urls.py
+++ apps/telegram/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     # path('sn/', views.TelegramSNList.as_view(), name='listsn01'),
     path('sn/', views.PostToUserList.as_view(), name='listsn01'),
-    # path('<slug:sn>/', views.TelegramSNDetail.as_view(), name='TelegramSNDetail01'),
+    path('sn/<slug:sn>/', views.TelegramSNDetail.as_view(), name='TelegramSNDetail01'),
 
     path('user/', views.TelegramUserList.as_view(), name='listuser01'),
     path('user/<slug:telegram_id>/', views.TelegramUserDetail.as_view(), name='detailtelegramuser01'),
