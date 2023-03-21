4aae695242910e5e5dd834ee50b6499d6cb327e7
cfssapp <gohweihan21@gmail.com>
2023-03-21 11:59:22 +0800

    save

--- apps/telegram/admin.py
+++ apps/telegram/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TelegramUser, TelegramComment, TelegramSN
 
 # Register your models here.
+admin.site.register(TelegramUser)
+admin.site.register(TelegramComment)
+admin.site.register(TelegramSN)
