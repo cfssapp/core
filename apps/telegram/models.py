97e83fe77dbd4b4b605d928298679fb113f4728e
cfssapp <gohweihan21@gmail.com>
2023-03-21 11:54:18 +0800

    save

--- apps/telegram/models.py
+++ apps/telegram/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class TelegramUser(models.Model):
+    telegram_id = models.CharField(max_length=255)
+    
+
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.telegram_id
+    
+
+class TelegramComment(models.Model):
+    content = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True)
+
+    sn_id = models.CharField(max_length=100, blank=True, null=True)
+
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.content
+    
+
+class TelegramSN(models.Model):
+    sn = models.CharField(max_length=100, default="not set")
+    
+    comments = models.ManyToManyField(TelegramComment, blank=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.sn
