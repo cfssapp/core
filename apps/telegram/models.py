e0e329a14ca8b527984e8c7f5411b2e8fee290a3
cfssapp <gohweihan21@gmail.com>
2023-03-21 13:38:02 +0800

    save

--- apps/telegram/models.py
+++ apps/telegram/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class TelegramUser(models.Model):
     telegram_id = models.CharField(max_length=255)
-    request_count = models.IntegerField(blank=True, null=True)
+    request_count = models.IntegerField(default=0, blank=True, null=True)
 
     class Meta:
         ordering = ['-id']
